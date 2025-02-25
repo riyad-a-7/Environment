8b5edcbb7cadafae389a239e7fc11e4d7242650d
Riyad Abdullayev <riyadabdullayev57@gmail.com>
2025-02-25 15:13:25 +0400

    isleme ardicilligi main.py elave edildi

---
+++ main.py
@@ -0,0 +1,5 @@
+#pythonda environment ile ish
+print("Ilk once env fayli yuklenir . . .")
+print("Bunun ucun Terminalda ```pip install env`` yaziriq")
+print("Sonra terminalda ```python venv -m myenv``` yaziriq")
+print("Artiq siz Virtual environment`i istifade ede bilersiz . . . ")
